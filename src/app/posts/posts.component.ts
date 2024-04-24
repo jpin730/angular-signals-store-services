a22acaab71b0c54389a89224cd2918795e1611a8
Jaime Pineda <jpin730@pm.me>
2024-04-24 08:03:27 -0600

    feat: :sparkles: add posts component and service

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core'
+import { PostsService } from './posts.service'
+
+@Component({
+  selector: 'app-posts',
+  standalone: true,
+  templateUrl: './posts.component.html',
+  providers: [PostsService],
+})
+export class PostsComponent {
+  private readonly postsService = inject(PostsService)
+
+  posts = this.postsService.posts
+  loading = this.postsService.loading
+  error = this.postsService.error
+
+  getAllPosts() {
+    this.postsService.getAll().subscribe()
+  }
+}
---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,69 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { Injectable, computed, inject, signal } from '@angular/core'
+import { Observable, catchError, tap } from 'rxjs'
+
+interface State<T> {
+  data: T
+  loading: boolean
+  error: string | null
+}
+
+export interface Post {
+  id: number
+  title: string
+}
+
+type PostsState = State<Post[]>
+
+@Injectable()
+export class PostsService {
+  private readonly http = inject(HttpClient)
+  private _state = signal<PostsState>({
+    data: [],
+    loading: false,
+    error: null,
+  })
+
+  posts = computed(() => this._state().data)
+  loading = computed(() => this._state().loading)
+  error = computed(() => this._state().error)
+
+  getAll(): Observable<Post[]> {
+    this.setLoading(true)
+    return this.http
+      .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+      .pipe(
+        tap((data) => {
+          this.setData(data)
+        }),
+        catchError((error: HttpErrorResponse) => {
+          this.setError(error)
+          throw error
+        }),
+      )
+  }
+
+  private setData(data: Post[]) {
+    this._state.update((state) => ({
+      ...state,
+      data,
+      loading: false,
+      error: null,
+    }))
+  }
+
+  private setLoading(loading: boolean) {
+    this._state.update((state) => ({
+      ...state,
+      loading,
+    }))
+  }
+
+  private setError(error: HttpErrorResponse) {
+    this._state.update((state) => ({
+      ...state,
+      loading: false,
+      error: error.message,
+    }))
+  }
+}
